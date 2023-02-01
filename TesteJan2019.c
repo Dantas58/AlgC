8ed86267ac3f203a90193a9b74617ab9f90838fe
Phy <pedrodantas8@gmail.com>
2023-02-01 19:34:42 +0000

    Teste Janeiro 2019 - Ex 1 e 4

--- TesteJan2019.c
+++ TesteJan2019.c
@@ -74,4 +74,58 @@ int kmaior (int v[], int N, int k){ // meu deus o struggle foi real nao sei quem
 // melhor caso: os 5 elementos iniciais de v são os k-ésimos maiores já em forma de min-heap, nao é efetuada nenhuma troca -> O(1)
 
 // pior caso: os 5 elementos iniciais de v são os 5 mais pequenos e a min-heap está toda trocada, a partir do elemento k todos os elementos
-// de v são maiores que h[0] e estão em ordem crescente -> xd 
+// de v são maiores que h[0] e estão em ordem crescente -> xd
+
+
+/*
+4. Dado um grafo G um caminho {v0, v1, . . . , vn} diz-se um caminho simples sse nao contiver
+elementos repetidos. Defina uma funcao
+int simplePath (Graph g, int v[], int k) que testa se a sequencia {v[0],v[1],...,v[k-1]}
+é um caminho simples. A funcao deve verificar que se trata realmente de um caminho e que nao tem vertices repetidos.
+Indique, justificando, a complexidade da solucao apresentada.
+*/
+
+#define N 50
+typedef struct edge {
+int dest, cost;
+struct edge *next;
+} * Graph [N];
+
+
+
+int lookup(Graph g, int x){
+
+    int i = 0;
+    while(i<N){
+        if(g[i] == x) return i;
+        i++;
+    }
+    return -1;
+}
+
+
+int simplePath (Graph g, int v[], int k){   // nao faço ideia se funfa 
+
+    int i = 0;
+
+    for(i; i<k; i++){
+        int j = 0;
+        for(j; j<k; j++){
+            if(v[j]==v[i]) return -1;
+        }
+    }
+
+    i=0;
+    while(i+1<k){
+        j = lookup(g, v[i]);
+        if(j==-1) return -1;
+
+        while(g[j]){
+            if(g[j]->dest == v[i+1]) break;
+            g[j] = g[j]->next;
+        }
+        if(!g[j]) return -1;
+        i++;
+    }
+    return 1;
+}
